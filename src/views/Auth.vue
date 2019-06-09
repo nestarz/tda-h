e3671beb3f58cc33629ff60e7b362195f554b47d
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-06-09 23:19:04 +0200

    push

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -2,9 +2,9 @@
   <v-container text-xs-center fill-height>
     <v-layout align-center justify-center row fill-height>
       <v-flex xs12 sm6 md4>
-        <p class="font-weight-bold display-3">
+        <p class="font-weight-bold display-3 custom-font">
           TDA
-          <span class="font-weight-light display-4">/</span>H
+          <span class="font-weight-thin display-4 custom-font">/</span>H
         </p>
         <v-spacer></v-spacer>
         <router-view/>
@@ -18,3 +18,14 @@ export default {
   name: "auth"
 };
 </script>
+
+<style lang="scss" scoped>
+.custom-font {
+  font-family: CircularStd !important;
+  letter-spacing: -1px !important;
+
+  span {
+    margin: 0 -7px !important;
+  }
+}
+</style>
