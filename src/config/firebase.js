dca39c2cd3e6639b907b3a64df79aa3386a31b57
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-06-09 20:11:15 +0200

    update firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,13 @@ import 'firebase/auth';
 // !! THIS is SPECIFICALLY for demo purposes! YOU should remove this file,
 // and then rename `firebase.default.js` to `firebase.js` to use in your own project!
 firebase.initializeApp({
-  apiKey: 'AIzaSyCoyXBD6u0Ysru9Op1ZOZffXlPiZps8WK4',
-  authDomain: 'authdemo-vue.firebaseapp.com',
-  databaseURL: 'https://authdemo-vue.firebaseio.com',
-  projectId: 'authdemo-vue',
-  storageBucket: 'authdemo-vue.appspot.com',
-  messagingSenderId: '808683114304',
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 });
 
 export default firebase;
